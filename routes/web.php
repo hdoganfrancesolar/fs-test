6877308d6d081aeb12fa4a6a0836ce331268b73c
PatrickPoney <creative.huso.o@gmail.com>
2023-04-18 17:58:13 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
