1c5fc83cb9ef3e30670fee2645958a617f7f5109
PatrickPoney <creative.huso.o@gmail.com>
2023-04-19 12:24:50 +0200

    Initial commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/tableau-de-bord';
 
     /**
      * Create a new controller instance.
