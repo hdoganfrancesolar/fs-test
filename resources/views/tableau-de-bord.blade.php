dc568be85588c01155d014175651dd241eacb180
PatrickPoney <creative.huso.o@gmail.com>
2023-04-20 00:17:53 +0200

    Initial commit

--- resources/views/tableau-de-bord.blade.php
+++ resources/views/tableau-de-bord.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+
             <div class="card">
                 <div class="card-header">Tableau de bord</div>
 
@@ -17,6 +18,18 @@
                     Vous êtes connecté!
                 </div>
             </div>
+
+            <div class="card mt-3">
+                <div class="card-header">De quoi vous inspirer : )</div>
+
+                <div class="card-body">
+                    <citation-component
+                            citation="Le succès n'est pas final, l'échec n'est pas fatal. C'est le courage de continuer qui compte."
+                            auteur="Winston Churchill">
+                    </citation-component>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
